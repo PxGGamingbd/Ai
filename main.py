0806169b89714566a1cbb9d088102ce4c7ee9878
PxGGamingbd <99589521+PxGGamingbd@users.noreply.github.com>
2023-10-01 00:14:57 +0600

    Update main.py

--- main.py
+++ main.py
@@ -9,10 +9,8 @@ bot = Bot( command_prefix="Alexa", community_id="19852745" #put your comId
 
 @bot.command("")
 def chat(ctx: Context, message: str):
-    bid="162843&key=uyk4IpiAS3SSjWXF"
-    apikey="uyk4IpiAS3SSjWXF"
-    id="162843"
-    r = requests.get(url=f"http://api.brainshop.ai/get?bid={bid}&key={apikey}&uid={id}&msg={message}")
+
+    r = requests.get(url=f"http://api.brainshop.ai/get?bid={"162843&key=uyk4IpiAS3SSjWXF"}&key={"uyk4IpiAS3SSjWXF"}&uid={"162843"}&msg={message}")
     ans=r.json()['cnt']
     ctx.reply(ans)
 
